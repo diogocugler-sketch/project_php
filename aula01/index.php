d3f5b0a3853c1c154ade239c67a16899e46071d1
diogocugler-sketch <diogocugler@gmail.com>
2026-07-07 22:12:32 -0300

    k

--- aula01/index.php
+++
@@ -1,2 +0,0 @@
-<?php echo "<h1>Olá, Alunos!</h1>"; ?>
-<?php echo "<h1>genio da bola</h1>"; ?>
---
+++ aula01/lista de compra com foreach.php
@@ -0,0 +1,8 @@
+    <?php
+    $produtos = ["Arroz", "Feijão", "Macarrão"];
+        echo "<ul>";
+        foreach ($produtos as $itens);
+         { echo "<li>produto: $itens</li> ";
+    } "</ul>"
+
+    ?>
